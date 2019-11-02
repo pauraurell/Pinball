f8a8657507500927f1e0d196115ac4dbec91fbaf
oscarroyo4 <47557315+oscarroyo4@users.noreply.github.com>
2019-11-02 19:38:30 +0100

    motors to levers

--- Physics2_class4 - handout/ModulePlayer.cpp
+++ Physics2_class4 - handout/ModulePlayer.cpp
@@ -2,6 +2,7 @@
 #include "Application.h"
 #include "ModulePlayer.h"
 #include "ModuleRender.h"
+#include "ModuleInput.h"
 #include "ModulePhysics.h"
 
 ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled)
@@ -22,20 +23,38 @@ bool ModulePlayer::Start()
 	rightLever = { 267, 817};
 	joint1 = App->physics->CreateCircle(leftLever.x+11, leftLever.y+12, 5, b2_staticBody);
 	joint2 = App->physics->CreateCircle(rightLever.x+69, rightLever.y+12, 5, b2_staticBody);
-	leftLeverBody = App->physics->CreateRectangle(leftLever.x+46, leftLever.y+12, 80, 15, b2_dynamicBody);
-	rightLeverBody = App->physics->CreateRectangle(leftLever.x+160, leftLever.y+12, 80, 15, b2_dynamicBody);
+	/*
+	int Lever[20] = {
+	8, 3,
+	15, 3,
+	25, 8,
+	79, 40,
+	79, 45,
+	75, 46,
+	58, 41,
+	6, 22,
+	2, 16,
+	3, 8
+	};
+
+	leftLeverBody = App->physics->CreateChain(leftLever.x, leftLever.y, Lever, 20, b2_dynamicBody);
+	*/
+	leftLeverBody = App->physics->CreateRectangle(leftLever.x+50, leftLever.y+12, 80, 15, b2_dynamicBody);
+	rightLeverBody = App->physics->CreateRectangle(leftLever.x+155, leftLever.y+12, 80, 15, b2_dynamicBody);
 	//Joint left lever
 	b2RevoluteJointDef def;
 	def.Initialize(joint1->body, leftLeverBody->body, joint1->body->GetWorldCenter());
 	def.enableLimit = true;
 	def.lowerAngle = -0.25f * b2_pi;
 	def.upperAngle = 0.15f * b2_pi;
+	def.maxMotorTorque = 60;
 	leftJoint = (b2RevoluteJoint*)App->physics->world->CreateJoint(&def);
 	//Joint right lever
 	def.Initialize(joint2->body, rightLeverBody->body, joint2->body->GetWorldCenter());
 	def.enableLimit = true;
 	def.lowerAngle = -0.15f * b2_pi;
 	def.upperAngle = 0.25f * b2_pi;
+	def.maxMotorTorque = 60;
 	rightJoint = (b2RevoluteJoint*)App->physics->world->CreateJoint(&def);
 
 	return true;
@@ -53,9 +72,30 @@ bool ModulePlayer::CleanUp()
 update_status ModulePlayer::Update()
 {
 	update_status ret = UPDATE_ERROR;
+
+	if (App->input->GetKey(SDL_SCANCODE_LCTRL) == KEY_REPEAT) {
+		leftJoint->EnableMotor(true);
+		leftJoint->SetMotorSpeed(-50);
+	}
+	else {
+		leftJoint->SetMotorSpeed(0);
+		leftJoint->EnableMotor(false);
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_LALT) == KEY_REPEAT) {
+		rightJoint->EnableMotor(true);
+		rightJoint->SetMotorSpeed(50);
+	}
+	else {
+		rightJoint->SetMotorSpeed(0);
+		rightJoint->EnableMotor(false);
+	}
+
+
 	if (Draw()) {
 		ret = UPDATE_CONTINUE;
 	}
+
 	return ret;
 }
 
