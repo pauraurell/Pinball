6a0e368f637aba894026d576848ffb957d24809b
pauraurell <47743764+pauraurell@users.noreply.github.com>
2019-11-02 18:46:46 +0100

    Added module Fonts

--- Physics2_class4 - handout/Application.cpp
+++ Physics2_class4 - handout/Application.cpp
@@ -1,4 +1,3 @@
-
 #include "Module.h"
 #include "ModuleWindow.h"
 #include "ModuleRender.h"
@@ -8,6 +7,7 @@
 #include "ModulePlayer.h"
 #include "ModulePhysics.h"
 #include "ModuleSceneIntro.h"
+#include "ModuleFonts.h"
 
 #include "Application.h"
 
@@ -21,6 +21,7 @@ Application::Application()
 	player = new ModulePlayer(this);
 	scene_intro = new ModuleSceneIntro(this);
 	physics = new ModulePhysics(this);
+	fonts = new ModuleFonts(this);
 
 	// The order of calls is very important!
 	// Modules will Init() Start() and Update in this order
@@ -33,6 +34,7 @@ Application::Application()
 	AddModule(textures);
 	AddModule(input);
 	AddModule(audio);
+	AddModule(fonts);
 	
 	// Scenes
 	AddModule(scene_intro);
